9dd26c26cca759f10a07225c9de9146a5ca87bff
fedfontana <federico.fontana2000@gmail.com>
2023-03-13 22:03:33 +0100

    format

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -2,12 +2,12 @@
 	import { isLoggedIn } from '../stores';
 </script>
 
-<nav class="flex flex-row py-4 px-12 font-semibold justify-between items-center">
-	<a href="/" class="text-2xl hover:underline place-self-start mr-auto"> mmh...ok? </a>
+<nav class="flex flex-row py-6 px-16 justify-between items-center">
+	<a href="/" class="text-3xl font-bold hover:underline place-self-start mr-auto"> mmh...ok? </a>
 	{#if !$isLoggedIn}
 		<a
 			href="/login"
-			class="px-3 py-2 text-xl bg-blue-400 rounded-lg border-2 border-blue-900 hover:scale-95 transition-all shadow-lg"
+			class="px-3 py-2 text-xl bg-blue-400 font-semibold rounded-lg border-2 border-blue-900 hover:scale-95 transition-all shadow-lg"
 		>
 			login
 		</a>
