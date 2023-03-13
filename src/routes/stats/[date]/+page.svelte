88fe4e0c9c9b2aa55cc7677664c17fd1dedff0d2
fedfontana <federico.fontana2000@gmail.com>
2023-03-13 21:48:46 +0100

    remove leftover comment, add breakpoint to css

--- src/routes/stats/[date]/+page.svelte
+++ src/routes/stats/[date]/+page.svelte
@@ -5,13 +5,9 @@
 	export let data: PageData;
 </script>
 
-<!-- <pre>
-{JSON.stringify(data.day_entries, null, 2)}
-</pre> -->
-
 <div class="flex flex-col items-center gap-12 mt-12">
 	{#if data.day_entries.mmh.length > 0 || data.day_entries.ok.length > 0 || data.day_entries.mmh_ok.length > 0}
-		<div class="flex flex-row flex-wrap justify-center mx-12 gap-4 mx-auto w-fit">
+		<div class="flex flex-row flex-wrap justify-center mx-12 gap-4 md:mx-auto w-fit">
 			<DayEntries word="mmh" entries={data.day_entries.mmh} />
 			<DayEntries word="ok" entries={data.day_entries.ok} />
 			<DayEntries word="mmh ok" entries={data.day_entries.mmh_ok} />
