eb2f850233a187eafaf16f9820c0e88d1fd0669c
fedfontana <federico.fontana2000@gmail.com>
2023-03-13 21:43:05 +0100

    added input in home to see stats

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,32 @@
 <script>
-	import WordCounter from "./WordCounter.svelte";
-    export let data;
+	import { goto } from '$app/navigation';
+	import WordCounter from './WordCounter.svelte';
+	export let data;
 </script>
 
 <div class="flex flex-col gap-12 w-full mt-12">
-    <div class="flex md:flex-row flex-col md:gap-10 gap-12 mx-auto">   
-        <WordCounter endpoint="/api/mmh" word="mmh" initialCount={data["mmh"]}/>
-        <WordCounter endpoint="/api/ok" word="ok" initialCount={data["ok"]} />
-    </div>
-    <div>
-        <WordCounter endpoint="/api/mmh-ok" word="mmh-ok" initialCount={data["mmh-ok"]}/>
-    </div>
+	<div class="flex md:flex-row flex-col md:gap-10 gap-12 mx-auto">
+		<WordCounter endpoint="/api/mmh" word="mmh" initialCount={data['mmh']} />
+		<WordCounter endpoint="/api/ok" word="ok" initialCount={data['ok']} />
+	</div>
+	<WordCounter endpoint="/api/mmh-ok" word="mmh-ok" initialCount={data['mmh-ok']} />
+
+	<form class="mx-auto flex flex-col items-center gap-2 mt-24"
+        on:submit|preventDefault={(e) => {
+            //@ts-ignore-next-line
+            const formData = Object.fromEntries(new FormData(e.target));
+            console.log(formData.date);
+            goto(`/stats/${formData.date}`)
+        }}
+    >
+		<!-- TODO copy styles from other inputs/buttons/labels -->
+		<label for="date" class="text-xl"> See stats for: </label>
+		<input type="date" name="date" class="border-2 border-black px-3 py-2 rounded-md shadow-lg" />
+		<button
+            type="submit"
+			class="px-4 py-2 border-2 border-green-900 bg-green-500 rounded-lg shadow-xl hover:scale-95 transition-all"
+		>
+			Search
+		</button>
+	</form>
 </div>
--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -3,16 +3,13 @@
 </script>
 
 <nav class="flex flex-row py-4 px-12 font-semibold justify-between items-center">
-	<a href="/"
-		class="text-2xl hover:underline place-self-start mr-auto"
-	>
-		mmh...ok?
-	</a>
+	<a href="/" class="text-2xl hover:underline place-self-start mr-auto"> mmh...ok? </a>
 	{#if !$isLoggedIn}
 		<a
 			href="/login"
-			class="px-3 py-2 text-xl bg-blue-400 rounded-lg border-2 border-blue-900 hover:scale-95 transition-all"
-			>login</a
+			class="px-3 py-2 text-xl bg-blue-400 rounded-lg border-2 border-blue-900 hover:scale-95 transition-all shadow-lg"
 		>
+			login
+		</a>
 	{/if}
 </nav>
