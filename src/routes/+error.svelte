d94c4ce60e464aaff7131c72427d32e5e05cf4ee
fedfontana <federico.fontana2000@gmail.com>
2023-03-15 16:53:37 +0100

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex mx-auto w-fit items-center mt-12 md:mt-24 flex-col gap-12 md:gap-24 justify-center">
+    <div class="flex flex-col mx-auto w-fit items-center gap-4">
+        <h3 class="text-4xl font-bold">{$page.status}</h3>
+        <h2 class="text-5xl font-bold">{$page.error?.message ?? 'There was an error'}</h2>
+    </div>
+	<a class="text-2xl hover:underline font-semibold text-blue-500" href="/">return to home</a>
+</div>
