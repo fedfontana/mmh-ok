3f03fbde003bcf85f96d4c261641b05eb370c65c
fedfontana <federico.fontana2000@gmail.com>
2023-03-15 22:22:38 +0100

    page titles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import '$src/app.css';
+	import { TITLE } from '$src/constants';
 	import { isLoggedIn } from '$src/stores';
 	import type { LayoutServerData } from './$types';
 	import Navbar from './Navbar.svelte';
@@ -12,6 +13,12 @@
 		}
 	}
 </script>
+
+<svelte:head>
+	<title>
+		{TITLE}
+	</title>
+</svelte:head>
 
 <div class="dark:bg-neutral-800 dark:text-neutral-100 min-h-screen dark:[color-scheme:dark]">
 	<Navbar />
